5aeb93815422543766c8b1911bd98eed28c54cf9
Alexis Maiquez Murcia <almamu@almamu.com>
2022-12-05 09:30:13 +0100

    Added day 3 solution for C#
    Signed-off-by: Alexis Maiquez Murcia <almamu@almamu.com>

---
+++ csharp/AdventOfCode2022/Day3/Solution.cs
@@ -0,0 +1,63 @@
+﻿namespace AdventOfCode2022.Day3;
+
+public class Solution
+{
+    class Rucksacks
+    {
+        public string Compartment1 { get; init; }
+        public string Compartment2 { get; init; }
+        public string Contents => this.Compartment1 + this.Compartment2;
+
+        public char Compare (Rucksacks first, Rucksacks second)
+        {
+            string firstContents = first.Contents;
+            string secondContents = second.Contents;
+
+            return this.Contents.First (x => firstContents.Contains (x) && secondContents.Contains (x));
+        }
+        
+        public static explicit operator Rucksacks(string value)
+        {
+            return new Rucksacks ()
+            {
+                Compartment1 = value.Substring (0, value.Length / 2),
+                Compartment2 = value.Substring (value.Length / 2)
+            };
+        }
+
+        public char [] RepeatedElements => this.Compartment1.Where (x => this.Compartment2.Contains (x)).Distinct().ToArray ();
+    }
+    
+    private string [] ReadInputFile ()
+    {
+        return File.ReadAllLines ("Data/Day3/input");
+    }
+
+    private IEnumerable <Rucksacks> ReadRucksacks (string[] lines)
+    {
+        return lines.Select (x => (Rucksacks) x);
+    }
+
+    public void FirstPart ()
+    {
+        int value = ReadRucksacks (ReadInputFile ()).Sum (x => x.RepeatedElements.CalculatePoints ());
+
+        Console.WriteLine (value);
+    }
+
+    public void SecondPart ()
+    {
+        int value = ReadRucksacks (ReadInputFile ())
+            .Select ((x, index) => new {x, index})
+            .GroupBy (x => x.index / 3)
+            .Select (x =>
+            {
+                Rucksacks [] elements = x.Take (3).Select (x => x.x).ToArray ();
+
+                return elements [0].Compare (elements [1], elements [2]);
+            })
+            .CalculatePoints ();
+
+        Console.WriteLine (value);
+    }
+}
--- csharp/AdventOfCode2022/Program.cs
+++ csharp/AdventOfCode2022/Program.cs
@@ -9,3 +9,9 @@ AdventOfCode2022.Day2.Solution day2 = new AdventOfCode2022.Day2.Solution ();
 Console.WriteLine ("Day 2");
 day2.FirstPart ();
 day2.SecondPart ();
+
+AdventOfCode2022.Day3.Solution day3 = new AdventOfCode2022.Day3.Solution ();
+
+Console.WriteLine ("Day 3");
+day3.FirstPart ();
+day3.SecondPart ();
